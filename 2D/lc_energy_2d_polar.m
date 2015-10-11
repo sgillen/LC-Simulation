05f2d75b9ad0ed303ef7f31baed62625475cb45b
Sean Gillen <seangillen1887@gmail.com>
2015-10-11 19:37:03 -0400

    removed 2D directory (replaced with 2D_Cartesian and 2D_Polar

--- 2D/lc_energy_2d_polar.m
+++
@@ -1,8 +0,0 @@
-function Energy = lc_energy_2d_polar(matrix, K11, K22, K33, deltax, deltay)
-
-Np = matrix;
-
-[dNpdy, dNpdx] = gradient(Np, deltaz, deltay)
-
-Energy = (1/2).*K11.*(dNpdy.*cos(Np)+(-1).*dNpdx.*sin(Np)).^2+(1/2).*K33.*( ...
-  dNpdx.*cos(Np)+dNpdy.*sin(Np)).^2;
